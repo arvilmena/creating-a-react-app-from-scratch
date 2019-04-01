2552e8d6037384c5e1c3ad1a68703b6f3db7700d
Arvil Mena <arvilmena@gmail.com>
2019-04-01 23:07:37 +0800

    add comment to not delete the HMR statement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+/*  DON'T DELETE THIS CONDITION,
+    THIS WILL HELP FOR HOTMODULEREPLACEMENT
+ */
 if (module.hot) {
   module.hot.accept();
 }
+/*  DON'T DELETE THIS CONDITION */
 
 // import React from "react";
 // import ReactDOM from "react-dom";
